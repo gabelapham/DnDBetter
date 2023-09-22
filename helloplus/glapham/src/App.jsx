3e2207b3a9c5d8b8cbf6f7cbd2bfdf495a32d71e
gabelapham <gabelapham@gmail.com>
2023-09-22 08:46:41 -0400

    Update App.jsx

--- helloplus/glapham/src/App.jsx
+++ helloplus/glapham/src/App.jsx
@@ -9,24 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://www.youtube.com/watch?v=7ODcC5z6Ca0&ab_channel=GamingSoundFX" target="_blank">
+          <img src="https://emojiisland.com/cdn/shop/products/Crying_Emoji_Icon_2_1024x1024.png?v=1571606091" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://youtu.be/dQw4w9WgXcQ?si=9I1pkFsgNUIoyqXi" target="_blank">
+          <img src="https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji.png" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Web Dev is HARD.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Web Dev Tears = {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Support the Web Dev team by giving me a 100 on this assignment please.
       </p>
     </>
   )
