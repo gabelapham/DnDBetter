bdb847a39799a29f3556c9d6e43ee7e6ae43c833
gabelapham <gabelapham@gmail.com>
2023-10-14 21:12:51 -0400

    Issue #20 User Input for main stats

--- frontend/Stats_Page/src/App.jsx
+++ frontend/Stats_Page/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import stat_sheet from './assets/stats_sheet.png'
 import './App.css'
 
+function strAdd() {
+  
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -19,6 +23,14 @@ function App() {
         </label>
         <label>
           <input id="playname" name="playName" />
+        </label>
+        <label>
+          <input id="str" type="number" step="1" pattern="\d+" name="strInt" min="1" max="20" />
+          <input id="dex" type="number" step="1" pattern="\d+" name="dexInt" min="1" max="20" />
+          <input id="con" type="number" step="1" pattern="\d+" name="conInt" min="1" max="20" />
+          <input id="int" type="number" step="1" pattern="\d+" name="intInt" min="1" max="20" />
+          <input id="wis" type="number" step="1" pattern="\d+" name="wisInt" min="1" max="20" />
+          <input id="cha" type="number" step="1" pattern="\d+" name="chaInt" min="1" max="20" />
         </label>
       </div>
     </>
