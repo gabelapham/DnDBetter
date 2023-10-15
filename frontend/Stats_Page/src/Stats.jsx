98bc07b780f484d9d11e0bf71485dea9458d5c6b
gabelapham <gabelapham@gmail.com>
2023-10-15 01:37:47 -0400

    Issue #20 Redoing.. everything?
    I've realized that the way that I was setting up my stat input was not very good, so I'm doing it a different way with increment/decrement buttons instead of keyboard input

--- frontend/Stats_Page/src/Stats.jsx
+++ frontend/Stats_Page/src/Stats.jsx
@@ -1,27 +1,65 @@
+import { useState } from 'react'
 import './Stats.css'
+import './index.css'
 
 function Stats() {
+    const [strength, setStrength] = useState(0)
+    function setStrengthFunc(e) {
+        if (e.target.checked > 20) {
+            setStrength(20)
+        } else {
+            setStrength(e.target.checked)
+        }
+    }
+
+    const [strength_st_pb, setStr] = useState(false)
+    function setStrFunc(e) {
+        setStr(e.target.checked)
+    }
+
 return(
     <>
     <div>
+        <label>
+            <button id="str_inc" onClick={() => setStrength(strength + 1)}>
+                +
+            </button>
+            <button id="str_dec" onClick={() => setStrength(strength - 1)}>
+                -
+            </button>
+            <p id="str">
+                {strength}
+            </p>
+        </label>
     <label>
-        <input id="str" type="number" step="1" pattern="\d+" name="strInt" min="1" max="20" />
+        <input id="dex" type="number" step="1" pattern="\d+" min="1" max="20" />
     </label>
     <label>
-        <input id="dex" type="number" step="1" pattern="\d+" name="dexInt" min="1" max="20" />
+        <input id="con" type="number" step="1" pattern="\d+" min="1" max="20" />
     </label>
     <label>
-        <input id="con" type="number" step="1" pattern="\d+" name="conInt" min="1" max="20" />
+        <input id="int" type="number" step="1" pattern="\d+" min="1" max="20" />
     </label>
     <label>
-        <input id="int" type="number" step="1" pattern="\d+" name="intInt" min="1" max="20" />
+        <input id="wis" type="number" step="1" pattern="\d+" min="1" max="20" />
     </label>
     <label>
-        <input id="wis" type="number" step="1" pattern="\d+" name="wisInt" min="1" max="20" />
+        <input id="cha" type="number" step="1" pattern="\d+" min="1" max="20" />
     </label>
-    <label>
-        <input id="cha" type="number" step="1" pattern="\d+" name="chaInt" min="1" max="20" />
-    </label>
+    </div>
+    <div>
+        <input type="checkbox" id="st_pb_str" checked={strength_st_pb} onChange={setStrFunc}/>
+        <p id="str_st">
+            {strength} {/*{strength_st_pb ? +2 : +0}*/}
+        </p>
+        <input type="checkbox" id="st_pb_dex"/>
+        <input type="checkbox" id="st_pb_con"/>
+        <input type="checkbox" id="st_pb_int"/>
+        <input type="checkbox" id="st_pb_wis"/>
+        <input type="checkbox" id="st_pb_cha"/>
+    </div>
+    <div>
+        
     </div>
     </>
 )
