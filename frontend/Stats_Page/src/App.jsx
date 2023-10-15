c30eb365d630d9a00d7077fbbedb8e0262b811ff
gabelapham <gabelapham@gmail.com>
2023-10-14 22:12:18 -0400

    Update App.jsx

--- frontend/Stats_Page/src/App.jsx
+++ frontend/Stats_Page/src/App.jsx
@@ -22,17 +22,6 @@ function App() {
         <label>
           <input id="playname" name="playName" maxLength={15} />
         </label>
-        <label>
-          <input id="str" type="number" step="1" pattern="\d+" name="strInt" min="1" max="20" />
-          <input id="dex" type="number" step="1" pattern="\d+" name="dexInt" min="1" max="20" />
-          <input id="con" type="number" step="1" pattern="\d+" name="conInt" min="1" max="20" />
-          <input id="int" type="number" step="1" pattern="\d+" name="intInt" min="1" max="20" />
-          <input id="wis" type="number" step="1" pattern="\d+" name="wisInt" min="1" max="20" />
-          <input id="cha" type="number" step="1" pattern="\d+" name="chaInt" min="1" max="20" />
-        </label>
-          <output id="st_str">
-            
-          </output>
       </div>
     </>
   )
