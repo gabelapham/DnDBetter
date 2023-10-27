a7f40a7770aa5380275d60d7076ba6f1fd45e9f2
gabelapham <gabelapham@gmail.com>
2023-10-26 21:37:38 -0400

    replacing increment/decrement buttons with dropdowns #39

--- frontend/Stats_Page/src/Stats.jsx
+++ frontend/Stats_Page/src/Stats.jsx
@@ -89,117 +89,69 @@ function Stats() {
 
     function Mod(e) {
         switch(e) {
-            case 1:
+            case '1':
                 return(-5)
-            case 2:
-            case 3:
+            case '2':
+            case '3':
                 return(-4)
-            case 4:
-            case 5:
+            case '4':
+            case '5':
                 return(-3)
-            case 6:
-            case 7:
+            case '6':
+            case '7':
                 return(-2)
-            case 8:
-            case 9:
+            case '8':
+            case '9':
                 return(-1)
-            case 10:
-            case 11:
+            case '10':
+            case '11':
                 return(0)
-            case 12:
-            case 13:
+            case '12':
+            case '13':
                 return(+1)
-            case 14:
-            case 15:
+            case '14':
+            case '15':
                 return(+2)
-            case 16:
-            case 17:
+            case '16':
+            case '17':
                 return(+3)
-            case 18:
-            case 19:
-                return (+4)
-            case 20:
-                return (+5)
+            case '18':
+            case '19':
+                return(+4)
+            case '20':
+                return(+5)
             default:
                 break
         }
     }
 
     function setStrengthFunc(e) {
-        if (e > 20) {
-            setStrength(20)
-            return;
-        } else if (e < 1) {
-            setStrength(1)
-            return;
-        } else {
-            setStrength(e)
-        }
+        setStrength(e)
         setStrengthMod(Mod(e))
         return;
     }
     function setDexterityFunc(e) {
-        if (e > 20) {
-            setDexterity(20)
-            return;
-        } else if (e < 1) {
-            setDexterity(1)
-            return;
-        } else {
-            setDexterity(e)
-        }
+        setDexterity(e)
         setDexterityMod(Mod(e))
         return;
     }
     function setConstitutionFunc(e) {
-        if (e > 20) {
-            setConstitution(20)
-            return;
-        } else if (e < 1) {
-            setConstitution(1)
-            return;
-        } else {
-            setConstitution(e)
-        }
+        setConstitution(e)
         setConstitutionMod(Mod(e))
         return;
     }
     function setIntelligenceFunc(e) {
-        if (e > 20) {
-            setIntelligence(20)
-            return;
-        } else if (e < 1) {
-            setIntelligence(1)
-            return;
-        } else {
-            setIntelligence(e)
-        }
+        setIntelligence(e)
         setIntelligenceMod(Mod(e))
         return;
     }
     function setWisdomFunc(e) {
-        if (e > 20) {
-            setWisdom(20)
-            return;
-        } else if (e < 1) {
-            setWisdom(1)
-            return;
-        } else {
-            setWisdom(e)
-        }
+        setWisdom(e)
         setWisdomMod(Mod(e))
         return;
     }
     function setCharismaFunc(e) {
-        if (e > 20) {
-            setCharisma(20)
-            return;
-        } else if (e < 1) {
-            setCharisma(1)
-            return;
-        } else {
-            setCharisma(e)
-        }
+        setCharisma(e)
         setCharismaMod(Mod(e))
         return;
     }
@@ -280,7 +232,6 @@ function Stats() {
 return(
     <>
     <div id="core">
-        <div>
         <label name="Player Stats" />
             <select id="level_select" onChange={() => pbFunc(level_select.value)}>
                 <option value="1">1</option>
@@ -304,157 +255,210 @@ return(
                 <option value="19">19</option>
                 <option value="20">20</option>
             </select>
-        </div>
-        <div>
         <label name="Prof Bonus" />
             <p id="pb">
                 +{pb}
             </p>
-        </div>
         {/* CORE STATS */}
-        <div>
-        <label name="STR Stats">
-            <div>
-            <button id="str_inc" checked={strength} onClick={() => setStrengthFunc(strength + 1)}>
-                +
-            </button>
-            </div>
-            <div>
-            <button id="str_dec" checked={strength} onClick={() => setStrengthFunc(strength - 1)}>
-                -
-            </button>
-            </div>
-            <div>
+        <label name="STR Stats" />
+            <select defaultValue={strength} id="str_select" onChange={() => setStrengthFunc(str_select.value)}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+            </select>
             <p id="str">
                 {strength}
             </p>
-            </div>
-            <div>
             <p id="str_mod">
                 {strengthMod}
             </p>
-            </div>
-        </label>
-        </div>
-        <label name="DEX Stats">
-            <button id="dex_inc" checked={dexterity} onClick={() => setDexterityFunc(dexterity + 1)}>
-                +
-            </button>
-            <button id="dex_dec" checked={dexterity} onClick={() => setDexterityFunc(dexterity - 1)}>
-                -
-            </button>
+        <label name="DEX Stats" />
+            <select defaultValue={dexterity} id="dex_select" onChange={() => setDexterityFunc(dex_select.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
             <p id="dex">
                 {dexterity}
             </p>
             <p id="dex_mod">
                 {dexterityMod}
             </p>
-        </label>
-        <label name="CON Stats">
-            <button id="con_inc" checked={constitution} onClick={() => setConstitutionFunc(constitution + 1)}>
-                +
-            </button>
-            <button id="con_dec" checked={constitution} onClick={() => setConstitutionFunc(constitution - 1)}>
-                -
-            </button>
+        <label name="CON Stats" />
+            <select defaultValue={constitution} id="con_select" onChange={() => setConstitutionFunc(con_select.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
             <p id="con">
                 {constitution}
             </p>
             <p id="con_mod">
                 {constitutionMod}
             </p>
-        </label>
-        <label name="INT Stats">
-            <button id="int_inc" checked={intelligence} onClick={() => setIntelligenceFunc(intelligence + 1)}>
-                +
-            </button>
-            <button id="int_dec" checked={intelligence} onClick={() => setIntelligenceFunc(intelligence - 1)}>
-                -
-            </button>
+        <label name="INT Stats" />
+            <select defaultValue={intelligence} id="int_select" onChange={() => setIntelligenceFunc(int_select.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
             <p id="int">
                 {intelligence}
             </p>
             <p id="int_mod">
                 {intelligenceMod}
             </p>
-        </label>
-        <label name="WIS Stats">
-            <button id="wis_inc" checked={wisdom} onClick={() => setWisdomFunc(wisdom + 1)}>
-                +
-            </button>
-            <button id="wis_dec" checked={wisdom} onClick={() => setWisdomFunc(wisdom - 1)}>
-                -
-            </button>
+        <label name="WIS Stats" />
+            <select defaultValue={wisdom} id="wis_select" onChange={() => setWisdomFunc(wis_select.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
             <p id="wis">
                 {wisdom}
             </p>
             <p id="wis_mod">
                 {wisdomMod}
             </p>
-        </label>
-        <label name="CHA Stats">
-            <button id="cha_inc" checked={charisma} onClick={() => setCharismaFunc(charisma + 1)}>
-                +
-            </button>
-            <button id="cha_dec" checked={charisma} onClick={() => setCharismaFunc(charisma - 1)}>
-                -
-            </button>
+        <label name="CHA Stats" />
+        <select defaultValue={charisma} id="cha_select" onChange={() => setCharismaFunc(cha_select.value)}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+            </select>
             <p id="cha">
                 {charisma}
             </p>
             <p id="cha_mod">
                 {charismaMod}
             </p> 
-        </label>
-    {/* SAVING THROWS + PB CHECKBOXES */}
-        <div>
+        {/* SAVING THROWS + PB CHECKBOXES */}
         <p id="str_st">
             {strengthMod+(strength_st_pb*pb)}
         </p>
-        </div>
-        <div>
             <input type="checkbox" id="st_pb_str" checked={strength_st_pb} onChange={setStrFunc}/>
-        </div>
-        <div>
         <p id="dex_st">
             {dexterityMod+(dexterity_st_pb*pb)}
         </p>
-        </div>
-        <div>
         <input type="checkbox" id="st_pb_dex" checked={dexterity_st_pb} onChange={setDexFunc}/>
-        </div>
-        <div>
         <p id="con_st">
             {constitutionMod+(constitution_st_pb*pb)}
         </p>
-        </div>
-        <div>
         <input type="checkbox" id="st_pb_con" checked={constitution_st_pb} onChange={setConFunc}/>
-        </div>
-        <div>
         <p id="int_st">
             {intelligenceMod+(intelligence_st_pb*pb)}
         </p>
-        </div>
-        <div>
         <input type="checkbox" id="st_pb_int" checked={intelligence_st_pb} onChange={setIntFunc}/>
-        </div>
-        <div>
         <p id="wis_st">
             {wisdomMod+(wisdom_st_pb*pb)}
         </p>
-        </div>
-        <div>
         <input type="checkbox" id="st_pb_wis" checked={wisdom_st_pb} onChange={setWisFunc}/>
-        </div>
-        <div>
         <p id="cha_st">
             {charismaMod+(charisma_st_pb*pb)}
         </p>
-        </div>
-        <div>
         <input type="checkbox" id="st_pb_cha" checked={charisma_st_pb} onChange={setChaFunc}/>
-        </div>
 
         {/* SKILLS */}
             <p id="acrobatics">
