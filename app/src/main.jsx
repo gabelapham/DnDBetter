b3960b7632278451a4320c477cd51a039911ec6a
tylerjcatuncan <tcatunca@vols.utk.edu>
2023-11-02 11:30:12 -0400

    fixed theme ui crash

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ThemeUIProvider } from "theme-ui";
-import theme from "./index.css?inline"
+import "./index.css"
+// import { ThemeUIProvider } from "theme-ui";
+// import theme from "./index.css?inline"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeUIProvider theme={theme}>
+    {/* <ThemeUIProvider theme={theme}> */}
       <App />
-    </ThemeUIProvider>
+    {/* </ThemeUIProvider> */}
   </React.StrictMode>,
 )
