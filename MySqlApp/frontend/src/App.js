cfa897bbdf6fdf8767fb91fdb2091b392de18cb4
Cooper Sarphie <cooperalpaca@outlook.com>
2023-11-08 00:32:31 -0500

    making account system work on separate page to be integrated with other pages

---
+++ MySqlApp/frontend/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+  };
+  return (
+    <div className="App">
+      <h1>Login Page</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
