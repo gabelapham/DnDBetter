1b12bb8703ad7448500d7ce25d73b2b0df711ae8
Zichen <zfu7@vols.utk.edu>
2023-11-15 00:53:51 -0500

    #69 setted up the spell selection menu

--- app/src/frontend/Spell_Page/src/SpellApp.jsx
+++ app/src/frontend/Spell_Page/src/SpellApp.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 import './SpellApp.css';
 import SpellBackground from './components/SpellBackground';
-import Navbar from './components/Navbar';
 
 function App() {
 
