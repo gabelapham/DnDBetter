6abae4426f9d00efdb527a62c5fda4cc933fa8f6
A-Swoo18 <san6@utk.edu>
2023-11-14 23:24:56 -0500

    #73
    implemented the dice roller  to the spell page too

--- app/src/frontend/Spell_Page/src/SpellApp.jsx
+++ app/src/frontend/Spell_Page/src/SpellApp.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
-
+import React, { useState } from 'react';
 import './SpellApp.css';
 import SpellBackground from './components/SpellBackground';
 import Navbar from './components/Navbar';
+import SpellDiceRoller from './components/DiceRoller'; // Adjust the path based on your folder structure
 
 function App() {
+  const [isDicePopupOpen, setDicePopupOpen] = useState(false);
+
+  const toggleDicePopup = () => {
+    setDicePopupOpen(!isDicePopupOpen);
+  };
 
   return (
     <>
-        <br />
-        <div>
-            <SpellBackground />
-        </div>
+      <br />
+      <div>
+        <button onClick={toggleDicePopup}>Open Spell Dice Roller</button>
+        {isDicePopupOpen && <SpellDiceRoller onClose={() => setDicePopupOpen(false)} />}
+        <SpellBackground />
+        {/* Other components or content */}
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
