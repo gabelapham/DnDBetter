f57177df1129aae1215c2d544991195ce9356392
A-Swoo18 <san6@utk.edu>
2023-12-01 18:14:05 -0500

    sprint4  cleanup code
    code cleanup

--- app/src/frontend/Background_Page/src/ImageComponent.jsx
+++ app/src/frontend/Background_Page/src/ImageComponent.jsx
@@ -21,41 +21,7 @@ function ImageComponent({ userText, onUserTextChange }) {
   return (
     <div className="image-container">
       <img src={image} alt="Character Sheet" id="png"/>
-      {/* {positions.map((position, index) => {
-        let textareaStyle = {
-          fontSize: '20px', // Adjust the font size as needed
-          width: '180px',   // Default width for most text areas
-          height: '30px',  // Default height for most text areas
-        };
-
-        // Check if this is one of the last four text areas
-        if (index === positions.length - 4) {
-          textareaStyle.width = '360px';  // Adjust the width for the last four text areas
-          textareaStyle.height = '460px'; // Adjust the height for the last four text areas
-        }
-        if (index === positions.length - 3) {
-          textareaStyle.width = '330px';  // Adjust the width for the last four text areas
-          textareaStyle.height = '780px'; // Adjust the height for the last four text areas
-        }
-        if (index === positions.length - 2) {
-          textareaStyle.width = '850px';  // Adjust the width for the last four text areas
-          textareaStyle.height = '500px'; // Adjust the height for the last four text areas
-        }
-        if (index === positions.length - 1) {
-          textareaStyle.width = '850px';  // Adjust the width for the last four text areas
-          textareaStyle.height = '375px'; // Adjust the height for the last four text areas
-        }
-        return (
-          <div className="text-box" style={position} key={index}>
-            <textarea
-              style={textareaStyle}
-              placeholder={`Enter text ${index + 1}`}
-              value={userText[index]}
-              onChange={(e) => onUserTextChange(index, e.target.value)}
-            />
-          </div>
-        );
-      })} */}
+      {}
       <p contentEditable id="character_name"></p>
       <p contentEditable id="age"></p>
       <p contentEditable id="eyes"></p>
